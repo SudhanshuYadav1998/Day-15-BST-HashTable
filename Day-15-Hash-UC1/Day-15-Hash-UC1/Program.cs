469fc1dd314e394610914a5ee05b13bf1cfc99d7
SudhanshuYadav1998 <sy7040@gmail.com>
2022-08-01 17:59:06 +0530

    Hash-UC-1 file Added

---
+++ Day-15-Hash-UC1/Day-15-Hash-UC1/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace HashTableDemo
+{
+    class MyMapNode<K, V>
+    { // const and readonly
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);  
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+       
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        protected int GetArrayPosition(K key)
+        { 
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash table demo");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2th index value: " + hash2);
+        }
+    }
+}
